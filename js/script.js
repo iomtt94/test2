1039507eedce0f4f5424dc2d67976e60afdb4551
Bohdan Artemenko <iomtt94@gmail.com>
2024-02-15 17:15:33 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    // --------------SCROLL-------------------
+    $('a[href="#"]').on("touchend, click", function (e) {
+        e.preventDefault();
+        $("body,html").animate({
+                scrollTop: $(".toscroll").offset().top - 50
+            },
+            400
+        );
+    });
+
+    $(".to-comments").on("touchend, click", function (e) {
+        e.preventDefault();
+        $("body,html").animate({
+                scrollTop: $(".comments.comments__list").offset().top - 50
+            },
+            400
+        );
+    });
+});
+
+
+// send form
+const form = document.querySelector("#order_form");
+async function sendData() {
+  const formData = new FormData(form);
+
+  try {
+    const response = await fetch("https://webhook.site/480a6ad0-20f6-44cb-a26b-b2ec841d2097", {
+      method: "POST",
+      body: formData,
+    });
+    console.log(await response.json());
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  sendData();
+});
